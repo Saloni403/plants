4d4a12f7eb51bc986a2bdc0a9c748fde001f577f
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-12-02 17:57:01 +0530

    updated

--- web/js/script.js
+++ web/js/script.js
@@ -3,14 +3,14 @@ let menu = document.querySelector(".menu")
 let closemenu = document.querySelector(".closemenu")
 openmenu.addEventListener("click",(e)=>{
     e.stopPropagation();
-menu.style.left="0";
+menu.style.right="0";
 })
 closemenu.addEventListener("click",(e)=>{
     e.stopPropagation();
-menu.style.left="-350px";
+menu.style.right="-350px";
 })
 document.addEventListener("click", (e) => {
     if (!menu.contains(e.target) && !openmenu.contains(e.target)) {
-        menu.style.left = "-350px";
+        menu.style.right = "-350px";
     }
 });
