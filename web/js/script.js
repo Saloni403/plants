7f08115f86802b397b6b5414da977d5c89b59405
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-12-04 12:58:02 +0530

    changes

--- web/js/script.js
+++ web/js/script.js
@@ -4,10 +4,12 @@ let closemenu = document.querySelector(".closemenu")
 openmenu.addEventListener("click",(e)=>{
     e.stopPropagation();
 menu.style.right="0";
+document.body.style.overflow="hidden"
 })
 closemenu.addEventListener("click",(e)=>{
     e.stopPropagation();
 menu.style.right="-350px";
+document.body.style.overflow = "auto"; 
 })
 document.addEventListener("click", (e) => {
     if (!menu.contains(e.target) && !openmenu.contains(e.target)) {
